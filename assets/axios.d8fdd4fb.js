a2e785fe923b2d62f41048b60496919ddf5317fe
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-03-29 18:28:50 +0000

    deploy: 12cce7d27ef31e462c772fcaeea6ab32e5ff253e

---
+++ assets/axios.d8fdd4fb.js
@@ -0,0 +1,1 @@
+import{b as e,a}from"./index.28f6415a.js";const s=a.create({baseURL:"https://api.example.com"});var t=e(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=s,console.log("[booted] axios")});export{s as api,t as default};
---
+++ assets/canvas.ba4d69e2.js
@@ -0,0 +1,1 @@
+/* empty css                       */import{b as o}from"./index.28f6415a.js";var r=o(({app:a})=>{console.log("[booted] Helper")});export{r as default};
